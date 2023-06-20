94538d753d4303ad3f78ab0eb9d7cf04ba10eae0
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-20 14:40:45 +0400

    Started from scratch

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import App from './App'
-import Template from './Template'
+import App from './App.jsx'
 import './index.css'
-import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Template />}>
-        <Route index element={<App />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
