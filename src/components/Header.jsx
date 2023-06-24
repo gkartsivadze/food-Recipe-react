512d904b6fdc3436cb57e08bc01b026c34f715b1
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-06-24 19:59:41 +0400

    added menu auto collapse mobile devices after navigating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ function Header({ variable }) {
     return <nav>
         <h1 className="logo">Receptionera</h1>
         <div className="navigation" style={window.innerWidth < 950 && menuState ? {transform: "translate(0)"} : {}}>
-            <Link to="/">HOME</Link>
-            <Link to="/categories">CATEGORIES</Link>
-            <Link to="/search-page">SEARCH</Link>
-            <Link to="/favourites">FAVOURITES<span id="favourite_counter">
+            <Link onClick={() => setMenuState(false)} to="/">HOME</Link>
+            <Link onClick={() => setMenuState(false)} to="/categories">CATEGORIES</Link>
+            <Link onClick={() => setMenuState(false)} to="/search-page">SEARCH</Link>
+            <Link onClick={() => setMenuState(false)} to="/favourites">FAVOURITES<span id="favourite_counter">
                 {typeof(variable) == "object" ? variable.length : 0}
                 </span></Link>
         </div>
